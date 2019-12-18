f6b93e93bc80a2188550384f485e1252c1ea9682
Celine <celi.caurier@gmail.com>
2019-12-18 22:40:59 +0100

    End

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,37 @@
+$(document).ready(function () {
 
-  $(function () {
-    $('#myTab li:last-child a').tab('show')
-  });
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+            if (anchor.length) {
+                $("html, body").animate({scrollTop: anchor.offset().top}, 1500);
+            }
+        }
+    });
 
-console.log('coucou');
+    $('.works-fin').click(function () {
+        $('#modal-works').modal('show')
+
+    });
+
+    $('.btn-cuncat').mousemove(function () {
+        $('.btn-cuncat').removeClass('btn-inactif-modal').addClass('btn-actif-modal');
+        $('.btn-lorem').removeClass('btn-actif-modal').addClass('btn-inactif-modal');
+    });
+
+    $('.btn-lorem').mousemove(function () {
+        $('.btn-lorem').removeClass('btn-inactif-modal').addClass('btn-actif-modal');
+        $('.btn-cuncat').removeClass('btn-actif-modal').addClass('btn-inactif-modal');
+    });
+
+    
+
+})
+
+
+
+
