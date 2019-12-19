197049338d9a3601ba6e4a948c187a49f8b84955
Celine <celi.caurier@gmail.com>
2019-12-19 06:53:07 +0100

    indentation

--- js/animations.js
+++ js/animations.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
 
-    $("a[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
-            if (anchor.length) {
-                $("html, body").animate({scrollTop: anchor.offset().top}, 1500);
-            }
-        }
-    });
 
     $('.works-fin').click(function () {
         $('#modal-works').modal('show')
@@ -28,7 +16,6 @@ $(document).ready(function () {
         $('.btn-cuncat').removeClass('btn-actif-modal').addClass('btn-inactif-modal');
     });
 
-    
 
 })
 
